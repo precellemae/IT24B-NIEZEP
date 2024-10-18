b27deeb171ea3cf9e74f7f46840b473b463cab41
precellemae <precellemaenieze@gmail.com>
2024-10-18 19:02:19 +0800

    created a function to filter students based on a query, combining their names and program

--- IT24B-Nieze/appletmap/Dynamic Elements/applet-4.js
+++ IT24B-Nieze/appletmap/Dynamic Elements/applet-4.js
@@ -4,12 +4,12 @@ class StudentList {
         this.students = [];
         this.init();
     }
-
     async init() {
         await this.fetchData();
         this.renderStudentList(this.students); 
         this.bindSearchEvent();
     }
+
     async fetchData() {
         try {
             const response = await fetch(this.dataUrl);
@@ -18,37 +18,3 @@ class StudentList {
             console.error('Error fetching data:', error);
         }
     }
-
-renderStudentList(students) {
-    const studentSearchListContainer = document.getElementById('studentSearchList');
-    studentSearchListContainer.innerHTML = ''; 
-
-    students.forEach(student => {
-        studentSearchListContainer.innerHTML += `
-            <p> ${student.student_name}</p>
-            <p class="fw-light"> ${student.student_program} </p>
-            <hr>
-        `;
-    });
-}
-
-bindSearchEvent() {
-    const studentSearchBar = document.getElementById('studentSearchBar');
-
-    studentSearchBar.addEventListener('input', () => {
-        this.filterStudents(studentSearchBar.value);
-    });
-
-    this.renderStudentList(this.students);
-}
-filterStudents(query) {
-    const filteredStudents = this.students.filter(student => {
-        const fullName = `${student.student_name} ${student.student_program}`;
-        return fullName.toLowerCase().includes(query.toLowerCase());
-    });
-
-    this.renderStudentList(filteredStudents);
-    }
-}
-
-const studentList = new StudentList('applet-4.json');
