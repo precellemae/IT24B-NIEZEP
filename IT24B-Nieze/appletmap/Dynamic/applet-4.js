cc6e123765d42a37e4bbf7ea151c70092b20b590
precellemae <precellemaenieze@gmail.com>
2024-10-18 18:14:00 +0800

    created a asyn function

---
+++ IT24B-Nieze/appletmap/Dynamic/applet-4.js
@@ -0,0 +1,12 @@
+class StudentList {
+    constructor(dataUrl) {
+        this.dataUrl = dataUrl;
+        this.students = [];
+        this.init();
+    }
+    
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+        this.bindSearchEvent();
+    }
