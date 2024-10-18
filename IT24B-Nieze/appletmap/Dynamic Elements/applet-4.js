684225e33a3030db561ba3da48db181f701fcdd7
precellemae <precellemaenieze@gmail.com>
2024-10-18 19:07:54 +0800

    created a function that it can show students name, program

--- IT24B-Nieze/appletmap/Dynamic Elements/applet-4.js
+++ IT24B-Nieze/appletmap/Dynamic Elements/applet-4.js
@@ -39,3 +39,14 @@ class StudentList {
 
         this.renderStudentList(this.students);
     }
+    filterStudents(query) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+
+        this.renderStudentList(filteredStudents);
+    }
+}
+
+const studentList = new StudentList('applet-4.json');
