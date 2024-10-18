8e91538bead41f025d0704ffa4eae2b17a7bef06
precellemae <precellemaenieze@gmail.com>
2024-10-18 18:16:07 +0800

    created a async fetchdata

--- IT24B-Nieze/appletmap/Dynamic/applet-4.js
+++ IT24B-Nieze/appletmap/Dynamic/applet-4.js
@@ -4,9 +4,18 @@ class StudentList {
         this.students = [];
         this.init();
     }
-    
+
     async init() {
         await this.fetchData();
         this.renderStudentList(this.students); 
         this.bindSearchEvent();
     }
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+}
