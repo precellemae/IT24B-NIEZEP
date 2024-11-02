f666e722034b4c02a241a02dfdec213aa100109e
precellemae <precellemaenieze@gmail.com>
2024-11-02 15:17:17 +0800

    make a class using weather app

---
+++ IT24B-Nieze/appletmap/Open Weather/applet.js
@@ -0,0 +1,14 @@
+class WeatherApp {
+    constructor() {
+        this.apiKeyInput = document.getElementById('apiKeyInput');
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+        this.getLocationBtn = document.getElementById('getLocationBtn');
+        this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+        this.weatherIcon = document.getElementById('weatherIcon');
+        this.map = L.map('map').setView([0, 0], 2); 
