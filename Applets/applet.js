fee954b3b6550f42e8a97be68130751fb4fc2ab3
precellemae <precellemaenieze@gmail.com>
2024-11-07 00:06:11 +0800

    added changes

--- Applets/applet.js
+++ Applets/applet.js
@@ -77,4 +77,4 @@ class AppletRenderer {
         }
         
         const appletRenderer = new AppletRenderer('applet-container', 'searchApplet');
-        appletRenderer.fetchAppletData('applets.json');
+        appletRenderer.fetchAppletData('applet.json');
